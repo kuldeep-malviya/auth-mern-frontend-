6ed48edf626bc5696ebec3cce78d6e674b9b2b7f
kuldeep-malviya <kuldeepmalviya0277@gmail.com>
2026-01-28 11:45:45 +0530

    Remove error throw for app render check

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-throw new Error("CHECKING WHICH APP.JSX RENDER IS USING");
 import React from 'react'
 import {BrowserRouter,Routes,Route, Navigate} from "react-router-dom"
 import Login from './pages/Login.jsx'
