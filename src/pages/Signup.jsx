6a27381fbc0ae8642beffefd1866f507b1766760
kuldeep-malviya <kuldeepmalviya0277@gmail.com>
2026-01-28 19:07:08 +0530

    Update signup URL to new endpoint

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
             return handelError("name ,email, password is required")
         }
         try {
-            const url = "https://auth-mern-49g9.onrender.com/auth/signup"
+            const url = "https://auth-mern-1-pw6l.onrender.com/auth/signup"
             const response = await fetch(url,{
                 method:"POST",
                 headers:{
